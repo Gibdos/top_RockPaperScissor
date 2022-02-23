02a0570745b9c8c8ff8a5c33b56bde0e0f265adc
Gibdos <simon.yousefi@protonmail.com>
2022-02-23 13:31:01 +0100

    basic rps game with basic ui

---
+++ script.js
@@ -0,0 +1,108 @@
+const computerChoice = ['Rock', 'Paper', 'Scissors']
+let playerSelectionQuestion;
+let computerScore = 0;
+let playerScore = 0;
+const paraPcChoice = document.querySelector('.pcScore');
+const paraNpcChoice = document.querySelector('.npcScore');
+const paraRound = document.querySelector('.round');
+const h1Finish = document.querySelector('.finish');
+let round = 0;
+
+function playRound(playerSelection, computerSelection) {
+    if (computerSelection === 'ROCK') {
+        if (playerSelection === 'SCISSORS') {
+            console.log(`Rock beats Scissors. The computer wins.`)
+            computerScore++;
+        }
+        else if (playerSelection === 'ROCK') {
+            console.log(`You both chose Rock. It's a tie`)
+        }
+        else {
+            console.log(`Paper beats Rock. You win!`)
+            playerScore++
+        }
+    }
+    if (computerSelection === 'SCISSORS') {
+        if (playerSelection === 'PAPER') {
+            console.log(`Scissors beats Paper. The computer wins.`)
+            computerScore++;
+        }
+        else if (playerSelection === 'SCISSORS') {
+            console.log(`You both chose Scissors. It's a tie`)
+        }
+        else {
+            console.log(`Rock beats Scissors. You win!`)
+            playerScore++
+        }
+    }
+    if (computerSelection === 'PAPER') {
+        if (playerSelection === 'ROCK') {
+            console.log(`Paper beats Rock. The computer wins.`)
+            computerScore++;
+        }
+        else if (playerSelection === 'PAPER') {
+            console.log(`You both chose Paper. It's a tie`)
+        }
+        else {
+            console.log(`Scissors beats Rock. You win!`)
+            playerScore++
+        }
+    }
+  }
+
+function game(playerChoice) {
+        let computerPlay = computerChoice[Math.floor(Math.random() * computerChoice.length)];
+        let computerSelection = computerPlay.toUpperCase();
+        let playerSelection = playerChoice.toUpperCase();
+        playRound(playerSelection, computerSelection);
+        console.log(`The score is:`);
+        console.log('Computer: ' + computerScore)
+        console.log('Player: ' + playerScore)
+        paraPcChoice.innerHTML = 'Player: ' + playerScore
+        paraNpcChoice.innerHTML = 'Computer: ' + computerScore
+        round++
+        paraRound.innerHTML = 'Round ' + round + ' / 5'
+    if (round === 5) {
+        finishGame()
+    }
+}
+
+function chooserock() {
+    playerSelectionQuestion = 'Rock'
+    game('Rock');
+}
+
+function choosepaper() {
+    playerSelectionQuestion = 'Paper'
+    game('Paper');
+}
+
+function choosescissors() {
+    playerSelectionQuestion = 'Scissors'
+    game('Scissors');
+}
+
+function finishGame() {
+    if (playerScore > computerScore) {
+        console.log('*** CONGRATULATIONS! YOU WON! ***')
+        computerScore = 0;
+        playerScore = 0;
+        round = 0;
+        h1Finish.innerHTML = '*** CONGRATULATIONS! YOU WON! ***'
+    } 
+    else if (playerScore < computerScore) {
+        console.log('*** SORRY BUT THE COMPUTER HAS WON ***')
+        computerScore = 0;
+        playerScore = 0;
+        round = 0;
+        h1Finish.innerHTML = '*** SORRY BUT THE COMPUTER HAS WON ***'
+    } else {
+        console.log(`*** IT'S A TIE! ***`)
+        computerScore = 0;
+        playerScore = 0;
+        round = 0;
+        h1Finish.innerHTML = `*** IT'S A TIE! ***`
+
+    }
+
+}
